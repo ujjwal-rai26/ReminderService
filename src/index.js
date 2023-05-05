971adb8456d8add30444f0638d5d4627f25eb539
ujjwal rai <ujjwalrai26092001@gmail.com>
2023-05-06 00:23:58 +0530

    added rabbitmq config

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ const bodyParser =require('body-parser');
 
 const { PORT }=require(`./config/serverConfig`);
 
+
 //const {sendBasicEmail}=require('./services/email-service');
 
 const TicketController=require('./controllers/ticket-controller');
 const jobs=require('./utils/job');
 
-const setupAndStartServer=()=>{
+const setupAndStartServer=async ()=>{
    const app=express();
 
    app.use(bodyParser.json());
    app.use(bodyParser.urlencoded({extended:true}));
+
+   const channel =await createChannel();
 
    app.post('/api/v1/tickets',TicketController.create);
 
